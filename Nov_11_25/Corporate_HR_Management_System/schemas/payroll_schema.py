7c890c9af4881625ed851cb36faa0dca28242619
code-YK <kpyadav2005@gmail.com>
2025-11-12 12:16:57 +0530

    Pydantic Schema for Payroll

---
+++ Nov_11_25/Corporate_HR_Management_System/schemas/payroll_schema.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, Field
+from datetime import date
+from config.enum import PayrollStatusEnum
+
+class PayrollBase(BaseModel):
+    base_salary : int = Field(..., gt=0, description="Base salary must be same as in employee record")
+    allowances : float = Field(..., ge=0, description="Allowances must be a non-negative number") 
+    deductions : float = Field(..., ge=0, description="Deductions must be a non-negative number")
+    net_pay : float = Field(..., gt=0, description="Net pay must be a positive number")
+    status : PayrollStatusEnum = Field(..., description="Status of the payroll(pending, paid)")
+    pay_date : date = Field(..., description="Pay date")
+
+    class Config:
+        orm_mode = True
+
+class PayrollCreate(PayrollBase):
+    emp_id : int = Field(..., description="The ID must refer to a valid employee")
+
+class PayrollRead(PayrollBase):
+    payroll_id : int
+    emp_id : int
