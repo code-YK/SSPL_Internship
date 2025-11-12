91d5cbb617af5ea7424464ad435a07e2ff441886
code-YK <kpyadav2005@gmail.com>
2025-11-12 12:12:19 +0530

    Models export definition

---
+++ Nov_11_25/Corporate_HR_Management_System/models/__init__.py
@@ -0,0 +1,11 @@
+from .department_model import Department
+from .employee_model import Employee
+from .payroll_model import Payroll
+from .leaveRecord_model import LeaveRecord
+
+__all__ = [
+    "Department",
+    "Employee",
+    "Payroll",
+    "LeaveRecord"
+]
