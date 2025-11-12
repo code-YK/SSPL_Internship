4381dbbff01b70926641187d802c089c4be9c3ce
code-YK <kpyadav2005@gmail.com>
2025-11-12 12:16:38 +0530

    Pydantic Schema for Employee

---
+++ Nov_11_25/Corporate_HR_Management_System/schemas/employee_schema.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel, Field, EmailStr
+from datetime import date
+from config.enum import GenderEnum
+
+class EmployeeBase(BaseModel):
+    name : str = Field(..., min_length=3, max_length=100)
+    email : EmailStr = Field(..., description="Email must be a valid email address")
+    gender : GenderEnum
+    hire_date : date = Field(..., description="Hire date in YYYY-MM-DD format")
+    salary : int = Field(..., gt=0, description="Salary must be a positive integer")
+
+    class Config:
+        orm_mode = True  # Enable ORM mode to work with SQLAlchemy models
+
+class EmployeeCreate(EmployeeBase):
+    dept_id : int = Field(..., gt=0, description="It must be a valid department ID")
+
+class EmployeeRead(EmployeeBase):
+    emp_id : int
+    dept_id : int | None = None
+
+class EmployeeUpdate(BaseModel):
+    name : str | None = Field(None, min_length=3, max_length=100)
+    email : EmailStr | None = Field(None, description="Email must be a valid email address")
+    gender : GenderEnum | None = None
+    hire_date : date | None = Field(None, description="Hire date in YYYY-MM-DD format")
+    dept_id : int | None = Field(None, gt=0, description="It must be a valid department ID")
+    salary : int | None = Field(None, gt=0, description="Salary must be a positive integer")
+    
+    
