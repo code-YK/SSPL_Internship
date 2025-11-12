14c5e0d017bb3f606fba3b36a1cd7fddb4ceb4f4
code-YK <kpyadav2005@gmail.com>
2025-11-12 12:15:53 +0530

    Pydantic Schema for Department

---
+++ Nov_11_25/Corporate_HR_Management_System/schemas/department_schema.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, Field
+
+class DepartmentBase(BaseModel):
+    name: str = Field(..., min_length=5, max_length=100)
+    budget: float = Field(..., gt=0, description="Budget must be a positive number")
+
+    class Config:
+        orm_mode = True # Enable ORM mode to work with SQLAlchemy models
+
+class DepartmentCreate(DepartmentBase):
+    manager_id: int = Field(..., gt=0, description="Manager ID must be a valid employee ID")
+
+class DepartmentRead(DepartmentBase):
+    dept_id: int
+    manager_id: int
+
