5c1002f6e944197d8144f0e0dd567b6ec37a3ef0
code-YK <kpyadav2005@gmail.com>
2025-11-27 16:27:41 +0530

    Import signatures are updated and enum for packagestatus is updated

--- Nov_25_to_Nov_28_25/Courier_Schedular_and_Tracking/config/__init__.py
+++ Nov_25_to_Nov_28_25/Courier_Schedular_and_Tracking/config/__init__.py
@@ -1,5 +1,12 @@
-from db_config import Base, get_db
-from logger_config import setup_logger
-from enum_config import packageStatus, pickupStatus
+from .db_config import Base, get_db, engine, SessionLocal
+from .logger_config import setup_logger
+from .enum_config import packageStatus, pickupStatus
 
-__all__ = ["Base", "get_db", "setup_logger", "packageStatus", "pickupStatus"]
+__all__ = ["Base", 
+           "get_db", 
+           "engine",
+           "SessionLocal",
+           "setup_logger", 
+           "packageStatus", 
+           "pickupStatus"
+           ]
--- Nov_25_to_Nov_28_25/Courier_Schedular_and_Tracking/config/enum_config.py
+++ Nov_25_to_Nov_28_25/Courier_Schedular_and_Tracking/config/enum_config.py
@@ -4,6 +4,10 @@ class packageStatus(Enum):
     CREATED = "created"
     IN_TRANSIT = "in transit"
     DELIVERED = "delivered"
+    PENDING = "pending"
+    SHIPPED = "shipped"
+    OUT_FOR_DELIVERY = "out for delivery"
+    CANCELLED = "cancelled"
 
 class pickupStatus(Enum):
     SCHEDULED = "scheduled"
