ee71bd9fe079ebdd81e4aaf75aedb86e02d2c313
code-YK <kpyadav2005@gmail.com>
2025-12-01 15:12:07 +0530

    Updated parameters of function

--- Nov_25_to_Nov_28_25/Courier_Schedular_and_Tracking/services/transaction_services.py
+++ Nov_25_to_Nov_28_25/Courier_Schedular_and_Tracking/services/transaction_services.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Session
-from models import Driver, Package, PickupRequest
+from models import Driver, Package, PickupRequest, Depot
 from schema import PackageCreate
-from datetime import datetime
+from datetime import datetime, timezone
 from config import setup_logger
 
 logger = setup_logger(__name__)
@@ -9,17 +9,20 @@ logger = setup_logger(__name__)
 class TransactionService:
 
     @staticmethod
-    def create_transaction(db: Session, package_create: PackageCreate, driver_id: int, scheduled_at) -> PickupRequest:
+    def create_transaction(db: Session, package_create: PackageCreate,depot_id: int, driver_id: int, scheduled_at) -> PickupRequest:
         """Create a package and associated pickup request transactionally."""
         try:
+            if db.query(Depot).filter(Depot.id == depot_id).first() is None:
+                raise ValueError(f"Depot with ID {depot_id} does not exist.")
+            
             # create a new package
             new_package = Package(
                 recipient_name=package_create.recipient_name,
                 address=package_create.address,
-                depot_id=package_create.depot_id,
+                depot_id=depot_id,
                 status=package_create.status,
-                created_at=datetime.utcnow(),
-                updated_at=datetime.utcnow(),
+                created_at=datetime.now(timezone.utc),
+                updated_at=datetime.now(timezone.utc),
                 tracking_number=package_create.tracking_number
             )
             db.add(new_package)
@@ -28,7 +31,7 @@ class TransactionService:
             #create a new pickup request (not all data)
             new_pickup_request = PickupRequest(
                 package_id=new_package.id,
-                requested_at=datetime.utcnow(),
+                requested_at=datetime.now(timezone.utc),
                 status="pending",
             )
 
@@ -37,7 +40,7 @@ class TransactionService:
                 raise ValueError(f"Driver with ID {driver_id} does not exist.")
             new_pickup_request.driver_id = driver_id
             new_pickup_request.scheduled_at = scheduled_at
-            
+
             db.add(new_pickup_request)
             db.commit()
             db.refresh(new_package)
