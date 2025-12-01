43011663da8750bf54c13e132477c30163cc63c2
code-YK <kpyadav2005@gmail.com>
2025-12-01 18:51:40 +0530

    Changed the default parameter for column (date type)

--- Nov_25_to_Nov_28_25/Courier_Schedular_and_Tracking/models/pickup_request_model.py
+++ Nov_25_to_Nov_28_25/Courier_Schedular_and_Tracking/models/pickup_request_model.py
@@ -1,13 +1,14 @@
 from sqlalchemy import Column, DateTime, Enum, Integer, String, ForeignKey
 from config import Base, pickupStatus
 from sqlalchemy.orm import relationship
+from datetime import datetime, timezone
 
 class PickupRequest(Base):
     __tablename__ = 'pickup_requests'
 
     id = Column(Integer, primary_key=True, index=True)
     package_id = Column(Integer, ForeignKey('packages.id'), nullable=False)
-    requested_at = Column(DateTime, nullable=False)
+    requested_at = Column(DateTime, nullable=False, default=datetime.now(timezone.utc))
     scheduled_at = Column(DateTime, nullable=True)
     driver_id = Column(Integer, ForeignKey('drivers.id'), nullable=True)
     status = Column(Enum(pickupStatus), default=pickupStatus.PENDING, nullable=False)
