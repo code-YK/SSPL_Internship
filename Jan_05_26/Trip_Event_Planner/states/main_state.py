d7b3fcdd490ac0ffb1da2663e901cd1abe90686c
code-YK <kpyadav2005@gmail.com>
2026-01-06 18:36:55 +0530

    Shared memory for the system

---
+++ Jan_05_26/Trip_Event_Planner/states/main_state.py
@@ -0,0 +1,31 @@
+from typing import TypedDict, List, Optional
+from typing_extensions import Annotated
+
+from langchain_core.messages import BaseMessage
+from langgraph.graph.message import add_messages
+
+from states.schemas import *
+
+class TripEventState(TypedDict, total=False):
+
+    # Conversation Memory
+    messages: Annotated[List[BaseMessage], add_messages]
+
+    # User Intent
+    user_intent: Optional[UserIntentModel]
+
+    # Research Stage
+    research_result: Optional[ResearchOutputModel]
+    research_review: Optional[ReviewDecisionModel]
+
+    # Planning Stage
+    itinerary: Optional[ItineraryModel]
+    planner_review: Optional[ReviewDecisionModel]
+
+    # Pricing Stage
+    pricing: Optional[PricingModel]
+    pricing_review: Optional[ReviewDecisionModel]
+
+    # Control Flags
+    current_stage: Optional[str]
+    final_approved: bool
