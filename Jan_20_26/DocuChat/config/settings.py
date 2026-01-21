0ac1635eb25f49ce7755f653fdd9d65ef4de53e5
code-YK <kpyadav2005@gmail.com>
2026-01-21 17:59:06 +0530

    Solved the variable name missmatch

--- Jan_20_26/DocuChat/config/settings.py
+++ Jan_20_26/DocuChat/config/settings.py
@@ -3,9 +3,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
-MODEL_NAME = os.getenv("MODEL_NAME", "groq-llama2-70b-chat")
+MODEL_NAME = os.getenv("GROQ_MODEL", "openai/gpt-oss-120b")
 DEFAULT_TEMPERATURE = float(os.getenv("DEFAULT_TEMPERATURE", 0.7))
 
 debug = os.getenv("DEBUG", "False").lower() in ("true", "1", "t")
 
-EMBEDDING_MODEL_NAME = os.getenv("EMBEDDING_MODEL_NAME", "sentence-transformers/all-MiniLM-L6-v2")
+EMBEDDING_MODEL_NAME = os.getenv("EMBEDDING_MODEL", "sentence-transformers/all-MiniLM-L6-v2")
