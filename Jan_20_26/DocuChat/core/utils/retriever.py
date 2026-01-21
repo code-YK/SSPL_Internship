d4eafb4d63a79a928587362c202c9a906cf1cb1f
code-YK <kpyadav2005@gmail.com>
2026-01-21 18:02:23 +0530

    Added the similarity search function

--- Jan_20_26/DocuChat/core/utils/retriever.py
+++ Jan_20_26/DocuChat/core/utils/retriever.py
@@ -9,3 +9,10 @@ def create_vector_store(doc_chunks: List[str]) -> FAISS:
     embeddings = HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL_NAME)
     vector_store = FAISS.from_documents(doc_chunks, embeddings)
     return vector_store
+
+def retrieve_similar_documents(vector_store: FAISS, query: str, k: int = 5) -> List[str]:
+    '''Retrieve similar documents from the vector store based on the query.'''
+
+    similar_docs = vector_store.similarity_search(query, k=k)
+    return similar_docs
+
